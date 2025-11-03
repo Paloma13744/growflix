52aa15aecb7886a5863e305ddde8b616e3939d60
Paloma Nascimento <paloma13744@gmail.com>
2025-11-03 16:48:29 -0300

    feat:home - em andamento

--- js/script.js
+++ js/script.js
@@ -1,8 +1,70 @@
 const movies = [
     {
         img: "https://img.youtube.com/vi/aJ-HZuLcKeA/maxresdefault.jpg",
-        title: "Conheça a Growdev",
+        title: "Growcast #00 | Conheça a Growdev",
         link: "https://youtu.be/aJ-HZuLcKeA?si=nnGGpWoVGiAcvwik",
         categoy: "growcast"
-    }
+    },
+    {
+        img: "https://img.youtube.com/vi/7OWT3lfHYvE/maxresdefault.jpg",
+        title: "Growcast #01 | As profissões do futuro",
+        link: "https://youtu.be/7OWT3lfHYvE?si=xCJGUJNtekFHVd49",
+        categoy: "growcast"
+    },
+    {
+        img: "https://img.youtube.com/vi/ci1eQGPK89o/maxresdefault.jpg",
+        title: "Growcast #02 | Como seguir a carreira internacional?",
+        link: "https://youtu.be/ci1eQGPK89o?si=xlTTF6IBRw0qo-UL",
+        categoy: "growcast"
+    },
+    {
+        img: "https://img.youtube.com/vi/WM7pGG7OicQ/maxresdefault.jpg",
+        title: "Growcast #03 | Superando os desafios da carreira de Dev!",
+        link: "https://youtu.be/WM7pGG7OicQ?si=g1NjazxKBO30Odmz",
+        categoy: "growcast"
+    },
+    {
+        img: "https://img.youtube.com/vi/2c8VOBZ160Y/maxresdefault.jpg",
+        title: "Agile Master: tudo sobre a profissão!",
+        link: "Growcast #04 | Agile Master: tudo sobre a profissão!",
+        categoy: "growcast"
+    },
+    {
+        img: "https://img.youtube.com/vi/mxnEm03cYPc/maxresdefault.jpg",
+        title: "Growcast #5 | Como iniciar na carreira em Desenvolvimento de Softwares?",
+        link: "https://youtu.be/mxnEm03cYPc?si=QOPZTvyNLbuIzgmB",
+        categoy: "growcast"
+    },
+    {
+        img: "https://img.youtube.com/vi/m7DyR07KrOE/maxresdefault.jpg",
+        title: "Growcast #06 | Mulheres e inclusão na tecnologia",
+        link: "https://youtu.be/m7DyR07KrOE?si=y5-kqJc0P_Wc8HYC",
+        categoy: "growcast"
+    },
+    {
+        img: "https://img.youtube.com/vi/vW8G9KPwRL0/maxresdefault.jpg",
+        title: "Growcast #07 | Desenvolvendo profissionais na área de TI",
+        link: "https://youtu.be/vW8G9KPwRL0?si=I_ymBs8hCCmHA0so",
+        categoy: "growcast"
+    },
+    {
+        img: "https://img.youtube.com/vi/_Ut2KqAqPyk/maxresdefault.jpg",
+        title: "Growcast #8 | A solução que está transformando o mercado de TI no Brasil",
+        link: "https://youtu.be/_Ut2KqAqPyk?si=29uTq3s6y-v5QEOy",
+        categoy: "growcast"
+    },
+    
+    {
+        img: "https://img.youtube.com/vi/dfDgs0Ive2Q/maxresdefault.jpg",
+        title: "Growcast #9 | Acompanhe com quem realmente entende, tudo sobre o mercado de trabalho no mundo da TI.",
+        link: "https://youtu.be/dfDgs0Ive2Q?si=7sfx7Ln-1i8u5pe_",
+        categoy: "growcast"
+    },
+    
+    
+   
+
+
+
+
 ]
