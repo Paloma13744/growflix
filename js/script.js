bf73fd849e11246de24765a484635f01fda56b17
Paloma Nascimento <paloma13744@gmail.com>
2025-11-06 18:56:39 -0300

    continuacao home ..

--- js/script.js
+++ js/script.js
@@ -1,73 +1,51 @@
-const movies = [
-    {
-        img: "https://img.youtube.com/vi/aJ-HZuLcKeA/maxresdefault.jpg",
-        title: "Growcast #00 | Conheça a Growdev",
-        link: "https://youtu.be/aJ-HZuLcKeA?si=nnGGpWoVGiAcvwik",
-        categoy: "growcast"
-    },
-    {
-        img: "https://img.youtube.com/vi/7OWT3lfHYvE/maxresdefault.jpg",
-        title: "Growcast #01 | As profissões do futuro",
-        link: "https://youtu.be/7OWT3lfHYvE?si=xCJGUJNtekFHVd49",
-        categoy: "growcast"
-    },
-    {
-        img: "https://img.youtube.com/vi/ci1eQGPK89o/maxresdefault.jpg",
-        title: "Growcast #02 | Como seguir a carreira internacional?",
-        link: "https://youtu.be/ci1eQGPK89o?si=xlTTF6IBRw0qo-UL",
-        categoy: "growcast"
-    },
-    {
-        img: "https://img.youtube.com/vi/WM7pGG7OicQ/maxresdefault.jpg",
-        title: "Growcast #03 | Superando os desafios da carreira de Dev!",
-        link: "https://youtu.be/WM7pGG7OicQ?si=g1NjazxKBO30Odmz",
-        categoy: "growcast"
-    },
-    {
-        img: "https://img.youtube.com/vi/2c8VOBZ160Y/maxresdefault.jpg",
-        title: "Agile Master: tudo sobre a profissão!",
-        link: "Growcast #04 | Agile Master: tudo sobre a profissão!",
-        categoy: "growcast"
-    },
-    {
-        img: "https://img.youtube.com/vi/mxnEm03cYPc/maxresdefault.jpg",
-        title: "Growcast #5 | Como iniciar na carreira em Desenvolvimento de Softwares?",
-        link: "https://youtu.be/mxnEm03cYPc?si=QOPZTvyNLbuIzgmB",
-        categoy: "growcast"
-    },
-    {
-        img: "https://img.youtube.com/vi/m7DyR07KrOE/maxresdefault.jpg",
-        title: "Growcast #06 | Mulheres e inclusão na tecnologia",
-        link: "https://youtu.be/m7DyR07KrOE?si=y5-kqJc0P_Wc8HYC",
-        categoy: "growcast"
-    },
-    {
-        img: "https://img.youtube.com/vi/vW8G9KPwRL0/maxresdefault.jpg",
-        title: "Growcast #07 | Desenvolvendo profissionais na área de TI",
-        link: "https://youtu.be/vW8G9KPwRL0?si=I_ymBs8hCCmHA0so",
-        categoy: "growcast"
-    },
-    {
-        img: "https://img.youtube.com/vi/_Ut2KqAqPyk/maxresdefault.jpg",
-        title: "Growcast #8 | A solução que está transformando o mercado de TI no Brasil",
-        link: "https://youtu.be/_Ut2KqAqPyk?si=29uTq3s6y-v5QEOy",
-        categoy: "growcast"
-    },
-    
-    {
-        img: "https://img.youtube.com/vi/dfDgs0Ive2Q/maxresdefault.jpg",
-        title: "Growcast #9 | Acompanhe com quem realmente entende, tudo sobre o mercado de trabalho no mundo da TI.",
-        link: "https://youtu.be/dfDgs0Ive2Q?si=7sfx7Ln-1i8u5pe_",
-        categoy: "growcast"
-    },
-    
-    
-   
+import { movies } from './movie.js';
+
+const rowGrowcastItems = document.getElementById("growcast-items");
+const rowWebinarItems = document.getElementById("webinar-items");
+const rowUxUiItems = document.getElementById("uxUi-items");
+const rowDiversosItems = document.getElementById("diversos-items");
+
+const growcastItems = movies.filter((movie) => movie.category === "Growcast [Episódios]");
+const webinarItems = movies.filter((movie) => movie.category === "Webinar em Flutter");
+const uxUiItems = movies.filter((movie) => movie.category === "Jornada UX/UI");
+const diversosItems = movies.filter((movie) => movie.category === "Diversos");
+
+const iframeMovie = document.getElementById("iframe-movie");
+const movieModalEl = document.getElementById("movie-modal");
+const movieModal = new bootstrap.Modal(movieModalEl);
 
 
 
+function renderItems(element, items) {
+    items.forEach((item) => {
+        element.innerHTML += `
+            <div class="col-12 col-sm-6 col-md-3 col-movie">
+                <div class="item-hover">
+                    <img src="${item.img}" alt="Imagem ilustrativa para ${item.title}" class="img-fluid">
+                        <p class="detail-movie" data-link="${item.link}" onclick="openMovie(this)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="white" class="bi bi-play-circle" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
+                                <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"></path>
+                            </svg>
+                        <span>${item.title}</span>
+                    </p>
+                </div>
+            </div>
+        `;
+    });
+};
 
-]
+function openMovie(element) {
+    const link = element.getAttribute("data-link");
+    console.log(link);
+    iframeMovie.innerHTML = `
+    <iframe src="${link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  `;
+    movieModal.show();
+}
+window.openMovie = openMovie;
 
-
-
+renderItems(rowGrowcastItems, growcastItems);
+renderItems(rowWebinarItems, webinarItems);
+renderItems(rowUxUiItems, uxUiItems);
+renderItems(rowDiversosItems, diversosItems);
